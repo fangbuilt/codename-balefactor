b94c8d6017a1dae29270de5cfe92ed696eded93c
Bintang Naufal <1bintangnaufal@gmail.com>
2025-11-27 07:42:35 +0000

    fix: sender

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,13 @@
+import { ResendOTP } from "./resendOTP";
+import { ResendOTPPasswordReset } from "./resendOTPPasswordReset";
+
 export default {
   providers: [
     {
       domain: process.env.CONVEX_SITE_URL,
       applicationID: "convex",
     },
+    ResendOTP,
+    ResendOTPPasswordReset
   ],
 };
